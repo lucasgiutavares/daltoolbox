4d78efbc2834916d3ec1beb440a50d6435a35448
lucasgiutavares <lucasgiutavares@gmail.com>
2024-10-28 16:31:58 -0300

    Correct sae_autoencoder.py

--- inst/python/sae_autoencoder.py
+++ inst/python/sae_autoencoder.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-class SAE_AutoencoderTS(Dataset):
+class SAE_TS(Dataset):
     def __init__(self, num_samples, input_size):
         self.data = np.random.randn(num_samples, input_size)
 
@@ -69,77 +69,121 @@ def sae_create(input_size, encoding_size, k_ae=3):
 
 
 #Train SAE
-def sae_train(autoencoder, train_loader, num_epochs = 1000, learning_rate = 0.001):
+def sae_train(sae, train_loader, val_loader, num_epochs = 1000, learning_rate = 0.001, return_loss=False):
     criterion = nn.MSELoss()
-    optimizer = optim.Adam(autoencoder.parameters(), lr=learning_rate)
+    optimizer = optim.Adam(sae.parameters(), lr=learning_rate)
+    
+    train_loss = []
+    val_loss = []
     
     for epoch in range(num_epochs):
-        running_loss = 0.0
-        for data in train_loader:
-            inputs, _ = data
-            inputs = inputs.float()
-            inputs = inputs.view(inputs.size(0), -1)
+        train_epoch_loss = []
+        val_epoch_loss = []
+        # Train
+        sae.train()
+        for train_data in train_loader:
+            train_input, _ = train_data
+            train_input = train_input.float()
             optimizer.zero_grad()
-            outputs = autoencoder(inputs)
-            loss = criterion(outputs, inputs)
-            loss.backward()
+            train_output= sae(train_input)
+            train_batch_loss = criterion(train_output, train_input)
+            train_batch_loss.backward()
             optimizer.step()
-            running_loss += loss.item()
+            train_epoch_loss.append(train_batch_loss.item())
             
-    return autoencoder
+            
+        # Validation
+        sae.eval()
+        for val_data in val_loader:
+            val_input, _ = val_data
+            val_input = val_input.float()
+            val_output = sae(val_input)
+            val_batch_loss = criterion(val_output, val_input)
+            val_epoch_loss.append(val_batch_loss.item())
+            
+        train_loss.append(np.mean(train_epoch_loss))
+        val_loss.append(np.mean(val_epoch_loss))
+  
+    if return_loss:
+      return sae, train_loss, val_loss
+    else:
+      return sae
 
 
-def sae_fit(stack, data, batch_size = 32, num_epochs = 1000, learning_rate = 0.001):
+def sae_fit(stack, data, batch_size = 32, num_epochs = 1000, learning_rate = 0.001, return_loss=False):
     batch_size = int(batch_size)
     num_epochs = int(num_epochs)
 
     #STEP 1 - Start fitting first k autoencoder using original input layer    
     array = data.to_numpy()
-    array = array[:, :, np.newaxis]
+    array = array[:, :]
     
-    ds = SAE_AutoencoderTS(array)
-    train_loader = DataLoader(ds, batch_size=batch_size)
-
-    #print(f'Fit ae_k{0}')
+    val_sample = sample(range(1, array.shape[0], 1), k=int(array.shape[0]*0.3))
+    train_sample = [v for v in range(1, array.shape[0], 1) if v not in val_sample]
+    
+    train_data = array[train_sample, :]
+    val_data = array[val_sample, :]
+    
+    ds_train = SAE_TS(train_data)
+    ds_val = SAE_TS(val_data)
+    train_loader = DataLoader(ds_train, batch_size=batch_size)
+    val_loader = DataLoader(ds_val, batch_size=batch_size)
+    
     ae_k1 = stack[0]
-    ae_k1 = sae_train(ae_k1, train_loader, num_epochs = num_epochs, learning_rate = learning_rate)
+    ae_k1 = sae_train(ae_k1, train_loader, val_loader, num_epochs = num_epochs, learning_rate = learning_rate, return_loss=False)
     ae_k_out = sae_encode_decode(ae_k1, data)
-    
-    
     
     #STEP 2 - Fit internal layers using outputs from previous layers
     internal = int(len(stack)-1)
     
     for k in range(1, internal):
-        #print(f'Fit ae_k{k}')
         
-        array = ae_k_out[:, :, np.newaxis]
-        ds = SAE_AutoencoderTS(array)
-        #ds = ae_k_out
-        train_loader = DataLoader(ds, batch_size=batch_size)
+        array = ae_k_out[:, :]
+        
+        val_sample = sample(range(1, array.shape[0], 1), k=int(array.shape[0]*0.3))
+        train_sample = [v for v in range(1, array.shape[0], 1) if v not in val_sample]
+        
+        train_data = array[train_sample, :]
+        val_data = array[val_sample, :]
+        
+        ds_train = SAE_TS(train_data)
+        ds_val = SAE_TS(val_data)
+        train_loader = DataLoader(ds_train, batch_size=batch_size)
+        val_loader = DataLoader(ds_val, batch_size=batch_size)
         
         ae_k = stack[k]
-        ae_k = sae_train(ae_k, train_loader, num_epochs = num_epochs, learning_rate = learning_rate)
+        ae_k = sae_train(ae_k, train_loader, val_loader, num_epochs = num_epochs, learning_rate = learning_rate, return_loss=False)
         ae_k_out = sae_encode_decode(ae_k, ae_k_out)
     
     #STEP 3 - Fit last k autoencoder
     #print(f'Fit ae_k{len(stack)-1}')
-    ds = SAE_AutoencoderTS(ae_k_out)
-    train_loader = DataLoader(ds, batch_size=batch_size)
-    autoencoder = stack[-1]
-    autoencoder = sae_train(autoencoder, train_loader, num_epochs = num_epochs, learning_rate = learning_rate)
+    val_sample = sample(range(1, ae_k_out.shape[0], 1), k=int(ae_k_out.shape[0]*0.3))
+    train_sample = [v for v in range(1, ae_k_out.shape[0], 1) if v not in val_sample]
     
-    return autoencoder
+    train_data = array[train_sample, :]
+    val_data = array[val_sample, :]
+    
+    ds_train = SAE_TS(train_data)
+    ds_val = SAE_TS(val_data)
+    train_loader = DataLoader(ds_train, batch_size=batch_size)
+    val_loader = DataLoader(ds_val, batch_size=batch_size)
+    sae = stack[-1]
+    if return_loss:
+      sae, train_loss, val_loss = sae_train(ae_k, train_loader, val_loader, num_epochs = num_epochs, learning_rate = 0.001, return_loss=return_loss)
+      return sae, train_loss, val_loss
+    else:
+      sae = sae_train(ae_k, train_loader, val_loader, num_epochs = num_epochs, learning_rate = 0.001, return_loss=return_loss)
+      return sae
 
 
-def sae_encode_data(autoencoder, data_loader):
+def sae_encode_data(sae, data_loader):
     # Encode the synthetic time series data using the trained autoencoder
     encoded_data = []
     for data in data_loader:
         inputs, _ = data
         inputs = inputs.float()
         inputs = inputs.view(inputs.size(0), -1)
-        encoded = autoencoder.encoder(inputs)
+        encoded = sae.encoder(inputs)
         encoded_data.append(encoded.detach().numpy())
         
     encoded_data = np.concatenate(encoded_data, axis=0)
@@ -147,32 +191,32 @@ def sae_encode_data(autoencoder, data_loader):
     return encoded_data
 
 
-def sae_encode(autoencoder, data, batch_size = 32):
+def sae_encode(sae, data, batch_size = 32):
     #Condition to check numpy array in internal stacked autoencoders
     if not isinstance(data, np.ndarray):
         array = data.to_numpy()
     else:
         array = data
         
-    array = array[:, :, np.newaxis]
+    array = array[:, :]
     
-    ds = SAE_AutoencoderTS(array)
+    ds = SAE_TS(array)
     train_loader = DataLoader(ds, batch_size=batch_size)
     
-    encoded_data = sae_encode_data(autoencoder, train_loader)
+    encoded_data = sae_encode_data(sae, train_loader)
     
     return(encoded_data)
 
 
-def sae_encode_decode_data(autoencoder, data_loader):
+def sae_encode_decode_data(sae, data_loader):
     # Encode the synthetic time series data using the trained autoencoder
     encoded_decoded_data = []
     for data in data_loader:
         inputs, _ = data
         inputs = inputs.float()
         inputs = inputs.view(inputs.size(0), -1)
-        encoded = autoencoder.encoder(inputs)
-        decoded = autoencoder.decoder(encoded)
+        encoded = sae.encoder(inputs)
+        decoded = sae.decoder(encoded)
         encoded_decoded_data.append(decoded.detach().numpy())
         
     encoded_decoded_data = np.concatenate(encoded_decoded_data, axis=0)
@@ -180,18 +224,18 @@ def sae_encode_decode_data(autoencoder, data_loader):
     return encoded_decoded_data
 
 
-def sae_encode_decode(autoencoder, data, batch_size = 32):
+def sae_encode_decode(sae, data, batch_size = 32):
     #Condition to check numpy array in internal stacked autoencoders
     if not isinstance(data, np.ndarray):
         array = data.to_numpy()
     else:
         array = data
     
-    array = array[:, :, np.newaxis]
+    array = array[:, :]
     
-    ds = SAE_AutoencoderTS(array)
+    ds = SAE_TS(array)
     train_loader = DataLoader(ds, batch_size=batch_size)
     
-    encoded_decoded_data = sae_encode_decode_data(autoencoder, train_loader)
+    encoded_decoded_data = sae_encode_decode_data(sae, train_loader)
     
     return(encoded_decoded_data)
